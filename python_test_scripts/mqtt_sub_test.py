75dce35b510de17767ef1232316e73ce27d02646
Alisha Machin <alisha.machin1316@gmail.com>
2025-03-01 16:17:01 +0000

    Testing to see whether we can use MQTT

---
+++ python_test_scripts/mqtt_sub_test.py
@@ -0,0 +1,18 @@
+import sys
+import paho.mqtt.client as mqtt
+
+# msg callback function
+def on_message(client, data, msg):#
+    print(f"{msg.topic}: {msg.payload.decode()}")
+
+client = mqtt.Client()
+client.on_message = on_message
+
+if client.connect(host="localhost", port=1883, keepalive=60) != 0:
+    print("Unable to connect to MQTT broker")
+    sys.exit(1)
+
+client.subscribe("test_topic")
+
+# blocking call - continuously receive messages
+client.loop_forever()
