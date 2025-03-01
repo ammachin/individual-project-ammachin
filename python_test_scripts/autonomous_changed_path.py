c877d4ce68a07197f3e383668841fd052c3859c6
ammachin <alisha.machin1316@gmail.com>
2025-03-01 14:08:53 +0000

    Drone now flying.

--- python_test_scripts/autonomous_changed_path.py
+++ python_test_scripts/autonomous_changed_path.py
@@ -2,7 +2,7 @@
 
 Fly to position coordinates as instructed by user.
 
-Test script (untested).
+Test script.
 
 TODO: Combine set path and changed path (use classes!)
 
@@ -15,11 +15,11 @@ import time
 from threading import Event
 
 import cflib.crtp
-from cflib.crazyflie import CrazyFlie
+from cflib.crazyflie import Crazyflie
 from cflib.crazyflie.log import LogConfig
-from cflib.crazyflie.syncCrazyflie import SyncCrazyFlie
+from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
 from cflib.utils import uri_helper
-from cflib.utils.reset_estimator import reset_estimator
+#from cflib.utils.reset_estimator import reset_estimator
 
 URI = uri_helper.uri_from_env(default="radio://0/80/2M/E7E7E7E7E7") #TODO: change to my CrazyFlie
 deck_attached_event = Event()
@@ -35,7 +35,7 @@ def log_pos_callback(timestamp, data, log_conf):
     position_estimate[1] = data["kalman.stateY"]
     position_estimate[2] = data["kalman.stateZ"]
 
-    print("Position: {%f, %f, %f}" % position_estimate[0], position_estimate[1], position_estimate[2])
+    #print(f"Position: ({position_estimate[0], position_estimate[1], position_estimate[2]})")
 
 
 """
@@ -65,12 +65,24 @@ def init_log_config(scf):
     log_conf.add_variable("kalman.stateY", "float")
     log_conf.add_variable("kalman.stateZ", "float")
 
-    scf.cf.log.addConfig(log_conf)
+    scf.cf.log.add_config(log_conf)
     log_conf.data_received_cb.add_callback(log_pos_callback)
 
     log_conf.start()
 
     return log_conf
+
+def take_off(cf, position):
+    take_off_time = 1.0
+    sleep_time = 0.1
+    steps = int(take_off_time / sleep_time)
+    vz = position[2] / take_off_time
+
+    print(f'take off at {position[2]}')
+
+    for i in range(steps):
+        cf.commander.send_velocity_world_setpoint(0, 0, vz, 0)
+        time.sleep(sleep_time)
 
 
 """
@@ -83,24 +95,29 @@ Parameters
     yaw: required rotation to fly initial to final
 """
 def fly_trajectory(scf, final_pos, yaw):
+
     # arm the crazyflie
     scf.cf.platform.send_arming_request(True)
     time.sleep(1.0)
 
-    print("Starting path")
-    for i in range(50):
-        scf.cf.commander.send_position_setpoint(position_estimate[0], final_pos[1], position_estimate[2])
-        time.sleep(0.1)
+    take_off(scf.cf, final_pos)
 
-    print("Flying to {%f, %f, %f}" % final_pos[0], final_pos[1], final_pos[2])
+    print(f"Position: ({final_pos[0], final_pos[1], final_pos[2]})")
     for i in range(50): # why 50??
-        scf.cf.commander.send_position_setpoint(final_pos[0], final_pos[1], final_pos[2], yaw)
+        print(f"Position (1st stage): ({position_estimate[0], position_estimate[1], position_estimate[2]})")
+        scf.cf.commander.send_position_setpoint(final_pos[0], final_pos[1], final_pos[2], 0.0)
         time.sleep(0.1)
 
     print("Landing")
     for i in range(50):
-        scf.cf.commander.send_position_setpoint(position_estimate[0], 0.0, position_estimate[2])
+        print(f"Position (Second stage): ({position_estimate[0], position_estimate[1], position_estimate[2]})")
+        scf.cf.commander.send_position_setpoint(final_pos[0], final_pos[1], 0.15, 0.0)
         time.sleep(0.1)
+
+    print(f"Position: ({position_estimate[0], position_estimate[1], position_estimate[2]})")
+
+    scf.cf.commander.send_stop_setpoint()
+    scf.cf.commander.send_notify_setpoint_stop()
 
 
 """
@@ -125,9 +142,9 @@ def calculate_yaw(init_pos, final_pos):
 Main function.
 """
 if __name__ == "__main__":
-    cflib.crtp.initi_drivers()
-    with SyncCrazyFlie(URI, cf=CrazyFlie(rw_cache="./cache")) as scf:
-        reset_estimator(scf)
+    cflib.crtp.init_drivers()
+    with SyncCrazyflie(URI, cf=Crazyflie(rw_cache="./cache")) as scf:
+        #reset_estimator(scf)
 
         log_conf = init_log_config(scf)
 
@@ -135,25 +152,30 @@ if __name__ == "__main__":
         scf.cf.param.add_update_callback(group="deck", name="bcLoco", cb=param_deck_loco)
         time.sleep(1)
 
-        if not deck_attached_event.wait(timeout=5):
-            print("Loco Positioning deck not attached")
-            sys.exit(1)
+        # if not deck_attached_event.wait(timeout=5):
+        #     print("Loco Positioning deck not attached")
+        #     sys.exit(1)
 
         print("Enter all coordinates in the format x.x\n")
         final_pos = []
-        final_pos.append = input("Enter x coordinate: ")
-        final_pos.append = input("Enter y coordinate: ")
-        final_pos.append = input("Enter z coordinate: ")
+        x = input("Enter x coordinate: ")
+        y = input("Enter y coordinate: ")
+        z = input("Enter z coordinate: ")
 
-        for pos in final_pos:
+        final_pos.append(x)
+        final_pos.append(y)
+        final_pos.append(z)
+
+        for pos in range(len(final_pos)):
             final_pos[pos] = float(final_pos[pos])
 
         # We don't actually care about y
         final_pos[1] = 2.0
 
+        # check which yaw we need
         yaw = calculate_yaw((position_estimate[0], 2.0, position_estimate[2]),
-                      final_pos[0], final_pos[1], final_pos[2])
+                      (final_pos[0], final_pos[1], final_pos[2]))
 
-        fly_trajectory(scf, final_pos, yaw)
+        fly_trajectory(scf, final_pos, yaw[0])
     
         log_conf.stop()
